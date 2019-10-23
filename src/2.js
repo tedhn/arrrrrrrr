cbc37320d365f0059f631d46d8ddf28669dab6a3
Ted <heinhtetnaing186@gmail.com>
2019-10-23 23:00:47 +0630

    'fkjung'

--- src/2.js
+++ src/2.js
@@ -1,47 +1,55 @@
-import React, { useEffect , useState} from 'react';
+import React, { useEffect } from 'react';
 
 let count = 0 ;
 let index = 0 ;
 let currentLetter = '';
+let letter = '';
 
 export default function Content(){
 
 	const texts = ['FM Hoes' , 'Raiderers' , 'Broke'];
 
-	let [letter , setLetter] = useState('');
-
-
-
 	const typing =()=>{
 		if(count === texts.length){
 			count = 0;
 		}
-	
 
 		currentLetter = texts[count];
-		console.log(currentLetter)
-		setLetter(currentLetter.slice(0, ++index))
-		console.log(index + ')' + letter)
-		if(letter.length === currentLetter.length){
-			index = 0 ;
-			count ++;
-		}
-		
+
+		letter = currentLetter.slice(0 , ++index);
+
 		document.querySelector('.typing').textContent = letter;
 
+		if(letter.length === currentLetter.length){
+
+			count ++ ;
+			index = 0 ;
+
+		}
+
+		setTimeout(typing , 400)
 	}
 
-	useEffect(
-		typing
-	,[])
+	const deleting =()=>{
+		if(letter.length === 0){
+			setTimeout(typing , 400)
+		}
+		else{
+			letter = letter.slice(0 , -1);
+			document.querySelector('.typing').textContent = letter;
+		}
+	}
+
+	useEffect(typing,[])
 
 	return(
 		<div className='Content'>
 			<div className="line1">
 				<div>We are</div>
 				<div className="typing"></div>
-			</div>		
-			<button onClick={typing}>Click me</button>
+				<button onClick={typing}>asdf</button>
+				<button onClick={deleting}>1234</button>
+			</div>	
 		</div>
 	)
 }
