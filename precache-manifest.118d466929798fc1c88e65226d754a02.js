97f33cae38ea9dca2707e4ec5b4d004cb3f1ca52
Ted <heinhtetnaing186@gmail.com>
2019-10-26 15:48:54 +0630

    Updates

---
+++ precache-manifest.118d466929798fc1c88e65226d754a02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3715fbb36b5b154da011d28d7c7e4d5",
+    "url": "/arrrrrrrr/index.html"
+  },
+  {
+    "revision": "645f0ee69ae3bd433889",
+    "url": "/arrrrrrrr/static/css/main.7f7c5373.chunk.css"
+  },
+  {
+    "revision": "acf12d91446555a82db4",
+    "url": "/arrrrrrrr/static/js/2.30c1afc8.chunk.js"
+  },
+  {
+    "revision": "645f0ee69ae3bd433889",
+    "url": "/arrrrrrrr/static/js/main.216d4810.chunk.js"
+  },
+  {
+    "revision": "0b60c5c61d891c9e1528",
+    "url": "/arrrrrrrr/static/js/runtime-main.ada0a0f0.js"
+  }
+]);
